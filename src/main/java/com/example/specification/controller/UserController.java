7ea6ac344609849d5e10a7dc5fbe31a5715c00f4
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2026-03-17 18:44:56 +0545

    feat : get user with affailioated course

--- src/main/java/com/example/specification/controller/UserController.java
+++ src/main/java/com/example/specification/controller/UserController.java
@@ -1,18 +1,15 @@
 package com.example.specification.controller;
 
 
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.specification.service.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.specification.model.usermodel;
 import lombok.RequiredArgsConstructor;
@@ -29,6 +26,16 @@ public class UserController {
             userService.saveUser(entity);
 
             return ResponseEntity.ok("success");
+        }
+
+        @GetMapping("/users")
+        public ResponseEntity<?> getAllUsers() {
+            return ResponseEntity.ok(userService.getAllUsers());
+        }
+
+        @GetMapping("/users/{id}")
+        public ResponseEntity<?> getUserById(@PathVariable Long id) {
+            return ResponseEntity.ok(userService.getUserById(id));
         }
 
 }
--- src/main/java/com/example/specification/repository/UserRepostory.java
+++ src/main/java/com/example/specification/repository/UserRepostory.java
@@ -1,10 +1,22 @@
 package com.example.specification.repository;
 
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.example.specification.model.usermodel;
 public interface UserRepostory extends JpaRepository<usermodel, Long>, JpaSpecificationExecutor<usermodel> {
+
+	@Override
+	@EntityGraph(attributePaths = {"courses"})
+	List<usermodel> findAll();
+
+	@Override
+	@EntityGraph(attributePaths = {"courses"})
+	Optional<usermodel> findById(Long id);
 
 }
