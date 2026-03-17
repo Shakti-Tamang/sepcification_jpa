7d5971a8b0ce2cfb79e8a1398f6aa3a4359854e1
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2026-03-17 16:53:58 +0545

    feat : save course

--- src/main/java/com/example/specification/controller/CourseController.java
+++ src/main/java/com/example/specification/controller/CourseController.java
@@ -1,7 +1,14 @@
 package com.example.specification.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.specification.model.Course;
+import com.example.specification.service.CourseService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,5 +19,13 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/courses")
 public class CourseController {
 
-    
+    private final CourseService courtsService;
+
+    @PostMapping("/savCourse")
+
+    public ResponseEntity<?> saveCourse(@RequestBody Course course, @RequestParam("userId") Long userId) {
+        courtsService.saveCourse(course, userId);
+        return ResponseEntity.ok().build();
+    }
+
 }
