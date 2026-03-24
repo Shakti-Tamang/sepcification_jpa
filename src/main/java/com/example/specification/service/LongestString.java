86b7d74f9aeeee5e5e2b7374a1b083afd9abcc77
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2026-03-24 14:52:41 +0545

    feat : longest string

---
+++ src/main/java/com/example/specification/service/LongestString.java
@@ -0,0 +1,22 @@
+package com.example.specification.service;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LongestString {
+
+
+
+    public void biggestAll(){
+
+        List<String>list=Arrays.asList("java","python","c++","javascript","ruby");
+        List<String>end=List.of("java","python","c++","javascript","ruby");
+
+
+        int a[]={1,2,5,4,3};
+
+        Arrays.sort(a);
+
+        System.out.println(a.toString());
+    }
+}
