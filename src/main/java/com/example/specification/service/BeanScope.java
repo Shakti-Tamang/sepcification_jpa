b111775863a55ae9973f4430ed4b9416a23bb74b
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2026-03-24 13:56:07 +0545

    feat : record for immutable

---
+++ src/main/java/com/example/specification/service/BeanScope.java
@@ -0,0 +1,4 @@
+package com.example.specification.service;
+
+public class BeanScope {
+}
---
+++ src/main/java/com/example/specification/service/RecordClass.java
@@ -0,0 +1,28 @@
+package com.example.specification.service;
+
+
+import com.example.specification.model.Course;
+import com.example.specification.model.usermodel;
+import com.example.specification.records.shaktirecords;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecordClass {
+
+    usermodel usermodel=new usermodel();
+
+
+
+
+
+    public void test(){
+
+        System.out.println(usermodel.getName());
+        shaktirecords shaktirecords1 = new shaktirecords("shakti", 1234567890L, 25);
+
+//        shaktirecords1.age();
+
+        System.out.println(shaktirecords1.toString());
+    }
+
+}
